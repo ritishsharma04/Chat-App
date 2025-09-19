012bda26f67f8ff3d02e0c9e0b097a993dfdcf4b
Ritish Sharma <ritishsharma04022006@gmail.com>
2025-09-19 23:32:52 +0530

    Setting To Upload This Project On Vercel

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
-import RightSideBar from '../components/RightSideBar'
+import RightSideBar from '../components/RightSidebar'
 import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
